5235a48ee490a97ca5b33c1676e264018e343716
aa-tan <a.tan@keio.jp>
2018-11-08 16:56:46 +0900

    Added controller for player

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+	public float speed;
+	float verticalSpeed;
+	Rigidbody2D block = new Rigidbody2D();
+	// Use this for initialization
+	void Start () {
+		block = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		verticalSpeed = Input.GetAxis("Vertical");
+		block.velocity = new Vector2(0, verticalSpeed) * speed;
+	}
+}
